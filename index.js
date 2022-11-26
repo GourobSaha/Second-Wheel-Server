62072b85a74d7b3739f818662f8f29514ab1eb40
Gourob Saha <gourobsaha009@gmail.com>
2022-11-26 15:45:56 +0600

    Seller Role API created

--- index.js
+++ index.js
@@ -104,6 +104,13 @@ async function run() {
             const query = { email }
             const user = await usersCollection.findOne(query);
             res.send({ isAdmin: user?.role === 'Admin' });
+        });
+
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isSeller: user?.role === 'Seller' });
         })
 
 
